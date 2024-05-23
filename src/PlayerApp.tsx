8717855001484fafa722992c2cc0dea4283a018e
Avik Paul <avikpauldev@gmail.com>
2024-05-23 16:02:03 +0530

    Fix Tauri rust config.

--- src/PlayerApp.tsx
+++ src/PlayerApp.tsx
@@ -3,7 +3,7 @@ function PlayerApp() {
   return (
     <>
       <h1>MIRO Player</h1>
-      On device media player for raspberry Pi device.
+      On device media player for Raspberry-Pi device.
     </>
   );
 }
